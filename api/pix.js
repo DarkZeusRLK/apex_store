e4e506ccddae7ec33df2dd0e8a3baebad3d5cc3b
João Pedro <fernandesjoaopedro622@gmail.com>
2025-12-28 17:00:56 -0300

    att

--- api/pix.js
+++ api/pix.js
@@ -5,9 +5,10 @@ export default async function handler(req, res) {
   const { valor, nome_produto } = req.query;
 
   // CONFIGURAÇÕES DO SEU PIX
-  const CHAVE_PIX = "SUA_CHAVE_AQUI"; // Pode ser CPF, Email, Telefone ou Aleatória
-  const NOME_RECEBEDOR = "SEU NOME OU LOJA";
-  const CIDADE_RECEBEDOR = "SAO PAULO";
+  const CHAVE_PIX =
+    "00020101021126330014br.gov.bcb.pix0111114434889765204000053039865802BR5919JOAO P F DE ALMEIDA6008IVAIPORA62070503***63040C59"; // Pode ser CPF, Email, Telefone ou Aleatória
+  const NOME_RECEBEDOR = "APEX STORE LTDA";
+  const CIDADE_RECEBEDOR = "Rio de Janeiro";
 
   if (!valor) return res.status(400).json({ error: "Valor não informado" });
 
